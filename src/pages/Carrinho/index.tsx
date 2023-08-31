120937ea7ebf566db3a13ad1d7ce662d106d84d7
JoaoPedroBispo <jopbispo83@gmail.com>
2023-08-31 08:54:01 -0300

    Atualização

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -7,21 +7,20 @@ import morango from "../../../assets/morango.png";
 import melancia from "../../../assets/melancia.png";
 import laranja from "../../../assets/laranja.png";
 import cereja from "../../../assets/cereja.png";
+import melao from "../../../assets/melao.png";
+import abacaxi from "../../../assets/abacaxi.png";
+import pitaya from "../../../assets/pitaya.png";
 
 const width = Dimensions.get("screen").width;
 
 export default function Carrinho() {
   return (
     <>
-      <Image source={topo} style={estilos.topo} />
+      <Image source={logo} style={estilos.topo} />
 
       <View style={estilos.carrinho}>
         <Text style={estilos.titulocarrinho}>Produtos em Estoque</Text>
 
-        <View style={estilos.fazenda}>
-          <Image style={estilos.imagemFazenda} source={logo} />
-          <Text style={estilos.nomefazenda}>Jaú Serve Supermercado</Text>
-        </View>
 
         <Text style={estilos.descricao}>
           Com a qualidade e e preço baixo que voçê precisa.
@@ -81,6 +80,39 @@ export default function Carrinho() {
             </Text>
           </View>
         </View>
+
+        <View style={estilos.divProduto}>
+          <Image style={estilos.imagemProduto} source={pitaya} />
+          <View>
+            <Text style={estilos.nomeProduto}>Pitaya</Text>
+            <Text style={estilos.preco}>R$ 9,90 kg</Text>
+            <Text style={estilos.descricao}>
+              Fazenda Pepe Delicinhas
+            </Text>
+          </View>
+        </View>
+
+        <View style={estilos.divProduto}>
+          <Image style={estilos.imagemProduto} source={melao} />
+          <View>
+            <Text style={estilos.nomeProduto}>Melão</Text>
+            <Text style={estilos.preco}>R$ 9,90 kg</Text>
+            <Text style={estilos.descricao}>
+              Fazenda Gigantão
+            </Text>
+          </View>
+        </View>
+
+        <View style={estilos.divProduto}>
+          <Image style={estilos.imagemProduto} source={abacaxi} />
+          <View>
+            <Text style={estilos.nomeProduto}>Abacaxi</Text>
+            <Text style={estilos.preco}>R$ 9,90 kg</Text>
+            <Text style={estilos.descricao}>
+              Fazenda Bacaxi
+            </Text>
+          </View>
+        </View>
       </View>
     </>
   );
@@ -92,18 +124,8 @@ const estilos = StyleSheet.create({
     height: (578 / 768) * width,
   },
 
-  titulo: {
-    fontSize: 24,
-    fontWeight: "bold",
-    lineHeight: 26,
-    width: "100%",
-    textAlign: "center",
-    color: "white",
-    position: "absolute",
-    padding: 16,
-  },
-
   titulocarrinho: {
+    left: 3,
     fontSize: 26,
     lineHeight: 42,
     color: "#464646",
@@ -115,47 +137,34 @@ const estilos = StyleSheet.create({
     paddingHorizontal: 16,
   },
 
-  nomefazenda: {
-    fontSize: 16,
-    lineHeight: 26,
-    marginLeft: 12,
-    fontFamily: "MontserratBold",
-  },
-
-  imagemFazenda: {
-    width: 82,
-    height: 52,
-  },
-
-  fazenda: {
-    flexDirection: "row",
-    paddingVertical: 12,
-  },
-
-  descricao: {
-    color: "#a3a3a3",
-    fontStyle: "italic",
-  },
-
   preco: {
+    left: 10,
     fontSize: 26,
     lineHeight: 42,
-    color: "#2a9f85",
-    marginTop: 8,
+    color: "#FF0000",
+
   },
 
   imagemProduto: {
     width: 92,
     height: 92,
+    borderRadius: 5,
   },
 
   nomeProduto: {
+    left: 10,
     fontSize: 18,
     lineHeight: 26,
   },
 
+  descricao: {
+    left: 10,
+    color: "#a3a3a3",
+    fontStyle: "italic",
+  },
+
   divProduto: {
     flexDirection: 'row',
-    marginTop: 25
+    marginTop: 20,
   }
 });

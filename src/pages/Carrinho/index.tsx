416e862a4c4bf3eaf18007c670ac7ba19f1fb46f
JoaoPedroBispo <jopbispo83@gmail.com>
2023-09-01 12:26:54 -0300

    Atualização

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  Dimensions,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import topo from "../../../assets/topo.png";
 import logo from "../../../assets/logo.png";
 import abacate from "../../../assets/abacate.png";
@@ -16,103 +23,105 @@ const width = Dimensions.get("screen").width;
 export default function Carrinho() {
   return (
     <>
+    
       <Image source={logo} style={estilos.topo} />
 
       <View style={estilos.carrinho}>
-        <Text style={estilos.titulocarrinho}>Produtos em Estoque</Text>
+        <Text style={estilos.titulo}>Produtos em Estoque</Text>
 
-
-        <Text style={estilos.descricao}>
+        <Text style={estilos.subtitulo}>
           Com a qualidade e e preço baixo que voçê precisa.
         </Text>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={cereja} />
-          <View>
-           <Text style={estilos.nomeProduto}>Cereja</Text>
-           <Text style={estilos.preco}>R$ 12,90 kg</Text>
-           <Text style={estilos.descricao}>
-            Fazenda Etec Cidade do Livro
-           </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={cereja} />
+            <View>
+              <Text style={estilos.nomeProduto}>Cereja</Text>
+              <Text style={estilos.preco}>R$ 12,90 kg</Text>
+              <Text style={estilos.descricao}>Loja Casa do Santana</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={morango} />
-          <View>
-           <Text style={estilos.nomeProduto}>Morango</Text>
-           <Text style={estilos.preco}>R$ 3,70 kg</Text>
-           <Text style={estilos.descricao}>
-            Fazenda Pepe Delicinhas
-           </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={morango} />
+            <View>
+              <Text style={estilos.nomeProduto}>Morango</Text>
+              <Text style={estilos.preco}>R$ 3,70 kg</Text>
+              <Text style={estilos.descricao}>Loja Pepe Delicinhas</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={melancia} />
-          <View>
-            <Text style={estilos.nomeProduto}>Melancia</Text>
-            <Text style={estilos.preco}>R$ 10,99 kg</Text>
-            <Text style={estilos.descricao}>
-              Fazenda Hortifruti
-            </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={melancia} />
+            <View>
+              <Text style={estilos.nomeProduto}>Melancia</Text>
+              <Text style={estilos.preco}>R$ 10,99 kg</Text>
+              <Text style={estilos.descricao}>Fazenda Hortifruti</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={laranja} />
-          <View>
-            <Text style={estilos.nomeProduto}>Laranja</Text>
-            <Text style={estilos.preco}>R$ 2,50 kg</Text>
-            <Text style={estilos.descricao}>
-              Fazenda Etec Cidade do Livro
-            </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={laranja} />
+            <View>
+              <Text style={estilos.nomeProduto}>Laranja</Text>
+              <Text style={estilos.preco}>R$ 2,50 kg</Text>
+              <Text style={estilos.descricao}>
+                Fazenda Etec Cidade do Livro
+              </Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={abacate} />
-          <View>
-            <Text style={estilos.nomeProduto}>Abacate</Text>
-            <Text style={estilos.preco}>R$ 9,90 kg</Text>
-            <Text style={estilos.descricao}>
-              Fazenda Bacate
-            </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={abacate} />
+            <View>
+              <Text style={estilos.nomeProduto}>Abacate</Text>
+              <Text style={estilos.preco}>R$ 9,90 kg</Text>
+              <Text style={estilos.descricao}>Fazenda Bacate</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={pitaya} />
-          <View>
-            <Text style={estilos.nomeProduto}>Pitaya</Text>
-            <Text style={estilos.preco}>R$ 9,90 kg</Text>
-            <Text style={estilos.descricao}>
-              Fazenda Pepe Delicinhas
-            </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={pitaya} />
+            <View>
+              <Text style={estilos.nomeProduto}>Pitaya</Text>
+              <Text style={estilos.preco}>R$ 9,90 kg</Text>
+              <Text style={estilos.descricao}>Loja Pepe Delicinhas</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={melao} />
-          <View>
-            <Text style={estilos.nomeProduto}>Melão</Text>
-            <Text style={estilos.preco}>R$ 9,90 kg</Text>
-            <Text style={estilos.descricao}>
-              Fazenda Gigantão
-            </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={melao} />
+            <View>
+              <Text style={estilos.nomeProduto}>Melão</Text>
+              <Text style={estilos.preco}>R$ 9,90 kg</Text>
+              <Text style={estilos.descricao}>Fazenda Gigantão</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
 
-        <View style={estilos.divProduto}>
-          <Image style={estilos.imagemProduto} source={abacaxi} />
-          <View>
-            <Text style={estilos.nomeProduto}>Abacaxi</Text>
-            <Text style={estilos.preco}>R$ 9,90 kg</Text>
-            <Text style={estilos.descricao}>
-              Fazenda Bacaxi
-            </Text>
+        <TouchableOpacity>
+          <View style={estilos.divProduto}>
+            <Image style={estilos.imagemProduto} source={abacaxi} />
+            <View>
+              <Text style={estilos.nomeProduto}>Abacaxi</Text>
+              <Text style={estilos.preco}>R$ 9,90 kg</Text>
+              <Text style={estilos.descricao}>Fazenda Bacaxi</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -124,11 +133,11 @@ const estilos = StyleSheet.create({
     height: (578 / 768) * width,
   },
 
-  titulocarrinho: {
-    left: 3,
+  titulo: {
+    left: 1,
     fontSize: 26,
     lineHeight: 42,
-    color: "#464646",
+    color: "#333333",
     fontFamily: "MontserratBold",
   },
 
@@ -139,10 +148,9 @@ const estilos = StyleSheet.create({
 
   preco: {
     left: 10,
-    fontSize: 26,
+    fontSize: 21,
     lineHeight: 42,
-    color: "#FF0000",
-
+    color: "#E00029",
   },
 
   imagemProduto: {
@@ -157,14 +165,22 @@ const estilos = StyleSheet.create({
     lineHeight: 26,
   },
 
+  subtitulo: {
+    left: 1,
+    color: "#4A4A4A",
+    fontStyle: "italic",
+  },
+
   descricao: {
     left: 10,
-    color: "#a3a3a3",
+    color: "#4A4A4A",
     fontStyle: "italic",
   },
 
   divProduto: {
-    flexDirection: 'row',
-    marginTop: 20,
-  }
+    backgroundColor: "#F7F7F7",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginTop: 15,
+  },
 });
